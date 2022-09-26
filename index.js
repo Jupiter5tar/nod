db7b1613d07bd975afc4af38b9dd6254d3bcd106
syk <sykclu@gmail.com>
2022-09-25 23:03:46 -0400

    Second commit

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const http = require('http')
-const port = 3000
+const fs = require('fs')
 
-const server = http.createserver(function(req, res) {
+const hostname = '127.0.0.1'
+const port = 3000;
 
+const server = http.createServer((req, res) => {
+
+  res.writeHead(200, {'Content-Type':'text/html'})
+  fs.readFile('index.html', function (error, data) {
+  if (error) {
+      res.writeHead(404)
+      res.write('Error: File Not Found')
+    } else {
+      res.write(data)
+    }
+  })
 })
 
-server.listen(port, function(error) {
-  if (error) {
-    console.log('Something went wrong', error)
-  } else {
-    console.log('Server is listening on port ' + port)
-  }
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
 })
