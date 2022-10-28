2cd7469c1f116a4af08c38a6b36c68e2f71d6183
Jupiter5tar <sykclu@gmail.com>
2022-10-27 23:11:48 -0400

    nod

--- index.js
+++ index.js
@@ -1,22 +1,70 @@
-const http = require('http')
-const fs = require('fs')
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 
-const hostname = '127.0.0.1'
-const port = 3000;
+const port = 8000;
+const directoryName = './pages';
+
+const types = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'application/javascript',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  gif: 'image/gif',
+  json: 'application/json',
+  xml: 'application/xml',
+};
+
+const root = path.normalize(path.resolve(directoryName));
 
 const server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
 
-  res.writeHead(200, {'Content-Type':'text/html'})
-  fs.readFile('pages/index.html', function (error, data) {
-  if (error) {
-      res.writeHead(404)
-      res.write('Error: File Not Found')
-    } else {
-      res.write(data)
+  const extension = path.extname(req.url).slice(1);
+  const type = extension ? types[extension] : types.html;
+  const supportedExtension = Boolean(type);
+
+  if (!supportedExtension) {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('404: File not found');
+    return;
+  }
+
+  let fileName = req.url;
+  if (req.url === '/') fileName = 'index.html';
+  else if (!extension) {
+    try {
+      fs.accessSync(path.join(root, req.url + '.html'), fs.constants.F_OK);
+      fileName = req.url + '.html';
+    } catch (e) {
+      fileName = path.join(req.url, 'index.html');
     }
-  })
-})
+  }
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+  const filePath = path.join(root, fileName);
+  const isPathUnderRoot = path
+    .normalize(path.resolve(filePath))
+    .startsWith(root);
+
+  if (!isPathUnderRoot) {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('404: File not found');
+    return;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('404: File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': type });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port 127.0.0.1:${port}`);
+});
