6c58bf1991e7387612fd0c2cff5e76077eb00d34
syk <sykclu@gmail.com>
2023-06-20 00:17:09 -0400

    aa

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const server = http.createServer();
-server.on('request', (request, response) => {
-	console.log('this is an incoming request');
-});
-
-server.listen(3000);
